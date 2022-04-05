0be2e4632e3c27e8da62399c02bbfeaa18d6e471
webdidacticat <50372848+webdidacticat@users.noreply.github.com>
2022-04-05 10:50:32 +0200

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,25 +3,29 @@
 ####
 #### Necesario tener instalado en el PC el servicio SMTP
 #### 
-
+from email.mime.multipart import MIMEMultipart
+from email.MIMEImage import MIMEImage
+from email.mime.text import MIMEText
 import smtplib
 
 def email():
+        # create message object instance
+        msg = MIMEMultipart()
         ### Codi per enviar un mail
-        from_addr = 'mail_username@gmail.com'
-        to = 'mail_desino@gmail.com'
-        subject = 'Subject: E-mail HTML enviado desde Python\n'
-        message = subject + """Hola!<br/> <br/>Este es un <b>e-mail</b> enviand$
-
+        msg['From'] = 'mail_username@gmail.com'
+        msg['To'] = 'mail_desino@gmail.com'
+        msg['Subject'] = 'E-mail HTML enviado desde Python'
+        message = 'Hola!<br/> <br/>Este es un <b>e-mail</b> enviado con Python'
+        msg.attach(MIMEText(message, 'plain')) ## Para enviar solo texto
+        msg.attach(MIMEImage(file("google.jpg").read())) ## Para enviar fichero adjunto
         # Reemplaza estos valores con tus credenciales de Google Mail
-        username = 'mail_username@gmail.com'
         password = 'password'
 
         try:
                 server = smtplib.SMTP('smtp.gmail.com:587')
                 server.starttls()
-                server.login(username, password)
-                server.sendmail(from_addr, to, message)
+                server.login(msg['From'], password)
+                server.sendmail(msg['From'],  msg['To'], msg.as_string())
                 print("Correo enviado")
         except:
                 print("Error: el mensaje no pudo enviarse.\nCompruebe que sendmail se encuentra instalado en su sistema")
